1cdc2c09888c9715add6b25a7a8a13a62fe33bd2
Krasi Georgiev <48542862+krasigeorgiev@users.noreply.github.com>
2020-04-02 18:41:06 +0300

    Add files via upload

---
+++ 19-gitHub-Forms-Request-Types-and-Routing/core/Request.php
@@ -0,0 +1,17 @@
+<?php 
+
+//responsible for fetching information about the current browser request
+
+class Request
+{
+
+	public static function uri()
+	{
+		return trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+	}
+
+	public static function method()
+	{
+		return $_SERVER['REQUEST_METHOD']; 
+	}
+}
---
+++ 19-gitHub-Forms-Request-Types-and-Routing/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+// 17 - Dry Up Your Views - tamplate engines like Blade
+
+require 'core/bootstrap.php';
+
+
+
+require Router::load('routes.php')
+	->direct(Request::uri(), Request::method());
+
